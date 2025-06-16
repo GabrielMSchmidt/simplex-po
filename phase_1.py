8530c909c8e7287567c4371c31ec75a7f179851e
GabrielMSchmidt <gabrielmarochischmidt@gmail.com>
2025-06-15 21:31:14 -0300

    Refatoração: Ajustando a Fase 1

--- phase_1.py
+++ phase_1.py
@@ -1,19 +1,6 @@
-def simplex():
-  max, l, c, var = initVar()
-  A, b, c = initMatrix(l, c, var)
-  if pre_fase_1(max, A, b, c):
-    fase_1(A, b, c, l)
-  else:
-    z = fase_2(A, b, c, l)
-
-
-def fase_1(max, A, b, c):
-  pass
-
-
-def pre_fase_1(max, A, b, c):
-  if max:
-    max = False
+def pre_phase_1(max_min, A, b, c, sig_ig):
+  if max_min:
+    max_min = False
     c = [x * -1 for x in c]
 
   for i, n in enumerate(b):
@@ -30,24 +17,28 @@ def pre_fase_1(max, A, b, c):
         case '≤':
           sig_ig[i] = '≥'
 
-  if (sig_ig.find(">") or sig_ig.find("≥") or sig_ig.find("=")) != -1:
+  if (">" or "≥" or "=") in sig_ig:
     return True
   else:
     return False
 
+
+def phase_1(max, A, b, c):
+  pass
+
+
+if __name__ == '__main__':
+  A = [[-1, 1, -1, 0, 0], [1, -1, 0, 1, 0], [-1, 1, 0, 0, 1]]
+  b = [-6, 4, 4]
+  c = [-1, -2, 0, 0, 0]
+  l = 3
+  sig_ig = ["≥", "≥", "≤"]
+  max_min = True
+
+  pre_phase_1(max_min, A, b, c, sig_ig)
 
   print(f"Sinais: {sig_ig}")
   print(f"A: {A}")
   print(f"b: {b}")
   print(f"c: {c}")
 
-
-
-max = True
-A = [[-1, 1, -1, 0, 0], [1, -1, 0, 1, 0], [-1, 1, 0, 0, 1]]
-b = [-6, 4, 4]
-c = [-1, -2, 0, 0, 0]
-l = 3
-sig_ig = ["≥", "≥", "≤"]
-
-pre_fase_1(max, A, b, c, l)
